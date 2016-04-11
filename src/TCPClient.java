b245b725efc07b9655f655f1e6ef1f13272b753b
seblankaufnix <nieling.s@gmail.com>
2016-04-11 13:53:58 +0200

    fibrotnatsochi

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,5 +1,26 @@
+import java.io.BufferedReader;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+
 /**
  * Created by User on 11.04.2016.
  */
 public class TCPClient {
+    public static void main(String[] args) throws Exception {
+        int count;
+        int countfib;
+        while(true) {
+            BufferedReader fromUser = new BufferedReader(new InputStreamReader(System.in));
+            Socket clientSocket = new Socket("localhost", 1234);
+            DataOutputStream toServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader fromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            count = Integer.parseInt(fromUser.readLine());
+            toServer.writeBytes(count + "\n");
+            countfib = Integer.parseInt(fromServer.readLine());
+            System.out.printf("fibrotnatschi number at position %3d is: %5d %n", count, countfib);
+        }
+    }
+
 }
--- src/TCPServer.java
+++ src/TCPServer.java
@@ -9,20 +9,21 @@ import java.net.Socket;
  * Created by User on 11.04.2016.
  */
 public class TCPServer {
-    public static void main(String[] args) throws IOException {
-        String client;
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(6789);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        while (true){
-            Socket connection = serverSocket.accept();
-            BufferedReader brFromClient = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            DataOutputStream dosFromClient = new DataOutputStream(connection.getOutputStream());
-            client = brFromClient.readLine();
-            System.out.println("Recieved: " + client);
+    public static void main(String[] args) throws Exception {
+        int inval;
+        int outval;
+        ServerSocket welcomeSocket = new ServerSocket(1234);
+
+        while(true){
+            Socket conSocket = welcomeSocket.accept();
+            BufferedReader inFromClient =
+                    new BufferedReader(new InputStreamReader(conSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(conSocket.getOutputStream());
+            inval = Integer.parseInt(inFromClient.readLine());
+            System.out.print("recieved: "+inval);
+            outval = Main.fibonacci(inval);
+            System.out.println("send: "+outval);
+            outToClient.writeBytes(outval + "\n");
         }
     }
 }
