f58a0ae5807fa880079bf813aa76b6121d7ea93f
seblankaufnix <nieling.s@gmail.com>
2016-04-11 13:08:47 +0200

    bla

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-/**
- * Created by User on 11.04.2016.
- */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("this is figabuette:");
+        for(int i = 1; i < 11; i++){
+            System.out.printf("value f%3d: %7d %n", i, fibonacci(i));
+        }
+    }
+
+    public static int fibonacci(int count){
+        return fib(0,1, count);
+    }
+
+    private static int fib(int lastvalue, int value, int count){
+        if(count <= 1){
+            return value;
+        } else {
+            return fib(value, lastvalue + value, --count);
+        }
+    }
 }
