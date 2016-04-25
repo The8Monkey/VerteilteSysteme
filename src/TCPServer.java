44c958e8e24394dfed1a5ed2e83348aec1d61ea3
The8Monkey <chstumpe@web.de>
2016-04-25 17:45:15 +0200

    client server

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -11,21 +11,28 @@ public class TCPServer {
     public static void main(String[] args) throws Exception {
         int inval;
         int outval;
-        ServerSocket welcomeSocket = new ServerSocket(1234);
+        DataOutputStream outToClient=null;
+        ServerSocket welcomeSocket = new ServerSocket(5678);
         Socket conSocket = null;
         while(true){
             try {
                 conSocket = welcomeSocket.accept();
                 BufferedReader inFromClient =
                         new BufferedReader(new InputStreamReader(conSocket.getInputStream()));
-                DataOutputStream outToClient = new DataOutputStream(conSocket.getOutputStream());
+                outToClient = new DataOutputStream(conSocket.getOutputStream());
                 inval = Integer.parseInt(inFromClient.readLine());
                 System.out.print("recieved: " + inval+ "\n");
-                outval = Fibonacci.fibonacci(inval);
-                System.out.println("send: " + outval);
-                outToClient.writeBytes(outval + "\n");
+                if(inval<=0 || inval >=10000){
+                    System.out.println("send: " + -2);
+                    outToClient.writeBytes(-2 + "\n");
+                }else {
+                    outval = Fibonacci.fibonacci(inval);
+                    System.out.println("send: " + outval);
+                    outToClient.writeBytes(outval + "\n");
+                }
             } catch(NumberFormatException e){
-                System.out.println("recieved int is not valid");
+                System.out.println("send: " + -1);
+                outToClient.writeBytes(-1 + "\n");
             } finally {
                 conSocket.close();
             }
