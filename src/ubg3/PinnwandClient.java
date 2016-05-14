f61910f7f99b39f9efa6c65a94b366e0c01ded33
seblankaufnix <nieling.s@gmail.com>
2016-05-15 01:51:13 +0200

    almost there

--- src/ubg3/PinnwandClient.java
+++ src/ubg3/PinnwandClient.java
@@ -1,11 +1,74 @@
 package ubg3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 /**
  * Created by nieli on 12-May-16.
  */
 public class PinnwandClient {
 
     public static void main(String[] args){
+
+        PinnwandInterface pinnwandServer = null;
+
+        try{
+            String name = "pinnwand-server";
+            int port = 1337;
+            String ip = "localhost";
+            Registry registry = LocateRegistry.getRegistry(ip, port);
+            pinnwandServer = (PinnwandInterface) registry.lookup(name);
+            System.out.println("connected to server: \"" + name + "\" \nPort: " + port + "\nIP: " + ip);
+        } catch (RemoteException | NotBoundException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader fromUser = new BufferedReader(new InputStreamReader(System.in));
+        loop: while(true){
+            try {
+                String in[] = fromUser.readLine().split(" ", 2);
+                switch(in[0].toLowerCase()){
+                    case "login":
+                        if(pinnwandServer.login(in[1]) == 1){
+                            System.out.println("successfully logged in.");
+                        } else {
+                            System.err.println("login failed");
+                        }
+                        break;
+                    case "put":
+                        if(pinnwandServer.putMessage(in[1])){
+                            System.out.println("entry \"" + in[1] + "\" successful");
+                        } else {
+                            System.err.println("entry failed, maybe forgot to log in?");
+                        }
+                        break;
+                    case "get":
+                        if(in.length < 2) {
+                            System.out.println("All Messages: \n");
+                            for (String msg : pinnwandServer.getMessages()) {
+                                System.out.println(msg);
+                            }
+                        }else{
+                            try{
+                                System.out.println("message at index: " + in[1] + ": " + pinnwandServer.getMessage(Integer.parseInt(in[1])));
+                            }catch (NumberFormatException e) {
+                                System.err.println("Error: Please enter valid Index");
+                            }
+                        }
+                    case "help":
+
+
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
--- src/ubg3/PinnwandServer.java
+++ src/ubg3/PinnwandServer.java
@@ -1,13 +1,15 @@
 package ubg3;
 
+import java.lang.reflect.Array;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by nieli on 5/9/2016.
+ * add this to VM-Options: "-Djava.security.policy=security.policy"
  */
 public class PinnwandServer extends UnicastRemoteObject implements PinnwandInterface {
 
@@ -17,7 +19,8 @@ public class PinnwandServer extends UnicastRemoteObject implements PinnwandInter
     protected final int lifeTime;
     protected final String nameOfService;
     private final String password;
-    protected ArrayList<String> messages;
+    protected HashMap<Long, String> messages;
+    private boolean loggedIn;
 
 
     public PinnwandServer(String nameOfService, String password, int lifeTime) throws RemoteException{
@@ -25,46 +28,76 @@ public class PinnwandServer extends UnicastRemoteObject implements PinnwandInter
         this.nameOfService = nameOfService;
         this.password = password;
         this.lifeTime = lifeTime;
+        messages = new HashMap<Long, String>();
+        loggedIn = false;
     }
 
     @Override
     public int login(String password) throws RemoteException {
-        if(this.password.equals(password)) return 1;
+        if(this.password.equals(password)){
+            loggedIn = true;
+            return 1;
+        }
+        loggedIn = false;
         return 0;
     }
 
     @Override
     public int getMessageCount() throws RemoteException {
+        updateMsgs();
         return messages.size();
     }
 
     @Override
     public String[] getMessages() throws RemoteException {
-        return (String[])messages.toArray();
+        updateMsgs();
+        return messages.values().toArray(new String[messages.values().size()]);
     }
 
     @Override
     public String getMessage(int index) throws RemoteException {
-        return messages.get(index);
+        updateMsgs();
+        return messages.values().toArray(new String[messages.values().size()])[index];
     }
 
     @Override
     public boolean putMessage(String msg) throws RemoteException {
-        if(messages.size() > MAX_MSGCOUNT | msg.length() > MAX_LENGTH) return false;
-        return messages.add(msg);
+        if(updateMsgs() && msg.length() < MAX_LENGTH && loggedIn){
+            messages.put(System.currentTimeMillis(), msg);
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    //returns true, is maximum count of messages isnt reached yet
+    private boolean updateMsgs() throws RemoteException {
+        Long current = System.currentTimeMillis();
+        for(Long t : messages.keySet()){
+            if(current - t > lifeTime){
+                System.out.println("message: \"" + messages.get(t) + "\" is outdated and will be removed");
+                messages.remove(t);
+            }
+        }
+        return messages.size() < MAX_MSGCOUNT;
     }
 
     public static void main(String[] args){
         if(System.getSecurityManager() == null){
             System.setSecurityManager(new SecurityManager());
         }
-
         try{
+            final String password ="password";
+            final int lifeTime = 10000;
+
             String name = "pinnwand-server";
-            PinnwandInterface pw = new PinnwandServer(name,"password",10);
-            Registry registry = LocateRegistry.getRegistry(5678);
-            registry.rebind(name,pw);
-            System.out.println("pinnwand bound");
+            PinnwandInterface pinnwand = new PinnwandServer(name,"password",lifeTime);
+
+            final int port = 1337;
+
+            Registry registry = LocateRegistry.createRegistry(port);
+            registry.rebind(name,pinnwand);
+            System.out.println("pinnwand bound to port: " + port);
         } catch (Exception e) {
             System.err.println("Something didnt work with the server");
             e.printStackTrace();
